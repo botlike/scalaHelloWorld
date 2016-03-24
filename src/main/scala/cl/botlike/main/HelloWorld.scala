0fef9a89b695e5968b758add9b394d96cdd116c5
cmontecinos <carlos.montecinos86@gmail.com>
2016-03-23 22:13:15 -0300

    Initial commit

---
+++ src/main/scala/cl/botlike/main/HelloWorld.scala
@@ -0,0 +1,12 @@
+package cl.botlike.main
+
+/**
+  * Created by cmontecinos on 23-03-16.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    print("Hell world from scala!!!!")
+  }
+
+}
